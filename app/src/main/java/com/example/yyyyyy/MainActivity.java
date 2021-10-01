063a5cc33249b5e5ae6e41518933fe111af7a62d
moko34 <75861228+moko34@users.noreply.github.com>
2021-10-01 18:46:44 +0000

    firts

--- app/src/main/java/com/example/yyyyyy/MainActivity.java
+++ app/src/main/java/com/example/yyyyyy/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.yyyyyy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
